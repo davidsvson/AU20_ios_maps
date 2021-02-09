2fd24b0fdb15f1641db96272eec9cec37e0789a0
David Svensson <davidsvson@gmail.com>
2021-02-09 09:07:19 +0100

    Initial Commit

---
+++ MapsLesson/MapsLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsLessonApp.swift
+//  MapsLesson
+//
+//  Created by David Svensson on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct MapsLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
